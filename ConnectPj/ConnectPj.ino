e5bbae5e84a00a1a6cc922cddd96f53b740c2d1b
Tatsu Lucas Ilya <lucas29liao@gmail.com>
2016-03-31 19:06:16 +0900

    Modify Vout range
    Modify Vout range to fit observed output

--- ConnectPj/ConnectPj.ino
+++ ConnectPj/ConnectPj.ino
@@ -2,12 +2,14 @@ void setup(){
   Serial.begin(9600);
 }
 
-  int pressure, pressureOut;
+int pressure, pressureOut;
 
 void loop(){
   pressure = analogRead(0);
   //pressureOut = map(pressure, 0, 1023, 0, 255);
-  pressureOut = map(pressure, 0, 800, 0, 255); 
+  //pressure = constrain(pressure, 501, 1023);
+  pressure = constrain(pressure, 501, 900);
+  pressureOut = map(pressure, 500, 900, 0, 255);
   Serial.write(pressureOut);
-  delay(100);
+  delay(1);
 }
